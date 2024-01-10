dd3056bbb30a6f5af83f87a9c78598b5b5b00a3d
Adam Barrow <adam@thebarrows.com>
2024-01-10 21:38:55 +0000

    Add Q2 challenge

---
+++ q2.py
@@ -0,0 +1,7 @@
+# API Request
+
+# Use the random user generator (https://randomuser.me/) to get a list of
+# 100 users (https://randomuser.me/api/?results=100) and output the following
+# files:
+# allusers.csv - a comma separated value representation of all users, including their first name, last name, street (made up of number and name), city, state, country, and email
+# country-counts.csv - a comma separated value file with two columns: country, count with a sumary of how many users are in the dataset for each country
